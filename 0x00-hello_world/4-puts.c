f74aa76dffdab34fe5e249e7cf6999f3130adf4e
Yomnamagdi <yomnamagdi177@gmail.com>
2023-04-23 17:53:07 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-i#include<stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -9,4 +9,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
