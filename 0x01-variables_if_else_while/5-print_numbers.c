39634e32443aa9ed7581e719d7ad8039796cdf1e
Yomnamagdi <yomnamagdi177@gmail.com>
2023-04-24 19:33:25 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,10 @@
  */
 
 int main(void) {
-    int i = 0;
-    while (i < 10) {
-        printf("%d\n", i);
-        i++;
-    }
+    int i;
+
+    for (i = 0; i < 10; i++) 
+	    printf("%d\n", i);
+    printf("\n");
     return 0;
 }
-
