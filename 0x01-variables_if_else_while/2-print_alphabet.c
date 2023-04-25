1edec98ecf5d397f340b37f2219de41e84c23b40
Yomnamagdi <yomnamagdi177@gmail.com>
2023-04-24 19:06:47 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charr;
+
+	for (charr = 'a'; ch <= 'z'; charr++)
+	{
+		putchar(charr);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
