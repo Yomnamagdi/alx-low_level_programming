173bd8792fffc311ba3ff74eeda0c49cea3f5945
Yomnamagdi <yomnamagdi177@gmail.com>
2023-04-26 18:15:22 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main: print_alphabet_x10 - prints 10 times the alphabet.
+ * @print_alphabet_x10: return 10 times the alphabet
+ * Return: 0
+ */
+void print_alphabet_x10(void)/* print_alphabet_x10 - prints 10 times alphabet*/
+
+{
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
+}
+}
