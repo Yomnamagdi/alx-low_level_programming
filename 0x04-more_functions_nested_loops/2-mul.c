4c95ca25df12e28516edaaa8dd9d391c9fc288d8
Yomnamagdi <yomnamagdi177@gmail.com>
2023-04-27 18:08:02 -0700

    multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * sum - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplicattion
+ */
+
+int sum(int a, int b)
+{
+	return (a * b);
+}
