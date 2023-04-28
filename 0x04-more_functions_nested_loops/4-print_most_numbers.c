5f1d06b030a1c7f4b69081069f13be034302c72b
Yomnamagdi <yomnamagdi177@gmail.com>
2023-04-27 18:30:32 -0700

    a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,12 @@
 
 void print_most_numbers(void)
 {
-	int  num;
+	char c;
 
-	for (num == 0; num  <= 9; num++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (num == 2 || num == 4)
-				continue;
-			_putchar(num+48);
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }
-
