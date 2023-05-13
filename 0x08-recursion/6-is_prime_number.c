8f6c366ed0cc11cb6650dcb03ddec163b4c79f4a
Yomnamagdi <yomnamagdi177@gmail.com>
2023-05-13 00:07:38 -0700

    d

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int actual_prime(int n, int x)
+{
+	if (x == 1)
+		return (1);
+	if (n % x == 0 && x > 0)
+		return (0);
+	return (actual_prime(n, x - 1));
+}
+
