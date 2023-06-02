e58d65d9da04c0f2340796775556b944347ee413
Yomnamagdi <yomnamagdi177@gmail.com>
2023-06-02 11:03:11 -0700

    advanced

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,9 @@
-section .data
-    message db "Hello, Holberton", 10, 0 ; string to be printed (with newline)
-section .text
-global main
-
+global   main
+	  extern    printf
 main:
-mov rdi, message ; load address of message into first argument register RDI
-xor eax, eax     ; clear EAX register to prepare for printf call
-call printf      ; call printf function with loaded arguments
-ret              ; return from main function and exit program
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
