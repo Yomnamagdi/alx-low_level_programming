cd2dba4696c597a1be8c6a6503a0c0f7eb266d37
Yomnamagdi <yomnamagdi177@gmail.com>
2023-06-02 11:08:48 -0700

    advanced

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,6 +4,7 @@
 
 /**
  * main - prints the minimum number of coins to
+ * make change for an amount of money
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -41,4 +42,3 @@ int main(int argc, char *argv[])
 	printf("%d\n", result);
 	return (0);
 }
-
